13e0a56b6a69abb1285807e52ed5430c5cd3ca1c
Ahmad-Abdou <kyoub1996@gmail.com>
2021-04-21 09:11:58 +0200

    Creating project

---
+++ src/main/java/com/ahmadabdou/studentregistrationrestapi/StudentRegistrationRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ahmadabdou.studentregistrationrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRegistrationRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmadabdou/studentregistrationrestapi/StudentRegistrationRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmadabdou.studentregistrationrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
